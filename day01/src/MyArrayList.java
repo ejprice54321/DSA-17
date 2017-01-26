fcf71a3b0109c67df16c237f22fa3faf53611027
ejprice54321 <emma.price@students.olin.edu>
2017-01-25 21:51:48 -0500

    adding in the pulled files from upstream

--- day01/src/MyArrayList.java
+++ day01/src/MyArrayList.java
@@ -3,33 +3,53 @@ public class MyArrayList {
 	private int size;
 
 	public MyArrayList() {
-		// TODO
+		elems = (Cow[]) new Object[10];
+		size = 0;
 	}
 
 	public MyArrayList(int capacity) {
-		// TODO
+		elems = (Cow[]) new Object[capacity];
+		size = 0;
 	}
 
 	public void add(Cow c) {
-		// TODO
+		if (size >= elems.length){
+			Cow[] bigger = (Cow[]) new Object[elems.length * 2];
+			System.arraycopy(elems, 0, bigger, 0, elems.length);
+			elems = bigger;
+		}
+		elems[size] = c;
+		size++;
 	}
 
 	public int size() {
-		// TODO
-		return -1;
+		return this.size;
 	}
 
 	public Cow get(int index) {
-		// TODO
-		return null;
+		if(index<0 || index >= size){
+			throw new IndexOutOfBoundsException();
+		}
+		return elems[index];
 	}
 
 	public Cow remove(int index) {
-		// TODO
-		return null;
+		Cow c = get(index);
+		for (int i = index; i < size-1; i++){
+			elems[i] = elems[i+1];
+		}
+		size--;
+		return c;
 	}
 
 	public void add(int index, Cow c) {
-		// TODO
+		if (index < 0 || index > size){
+			throw new IndexOutOfBoundsException();
+		}
+		add(c);
+		for (int i = size-1; i > index; i--){
+			elems[i] = elems[i-1];
+		}
+		elems[index] = c;
 	}
 }
