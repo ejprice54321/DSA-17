2e8508d1d3a4e245bcf5dd8b9610b96dbd06633c
ejprice54321 <emma.price@students.olin.edu>
2017-02-01 16:25:01 -0500

    updating day03

--- day03/src/your_code/MyStack.java
+++ day03/src/your_code/MyStack.java
@@ -5,11 +5,19 @@ import ADTs.StackADT;
 /**
  * An implementation of the Stack interface.
  */
-public class MyStack implements StackADT<Integer> {
+public class MyStack<T> implements StackADT<Integer> {
+    private T[] stack;
+    private int size;
+    private int top;
+
+    public MyStack(){
+        stack = new T[10];
+        top = -1;
+    }
 
     @Override
     public void push(Integer e) {
-        // TODO
+        stack
     }
 
     @Override
