ba8818381afce5035036b21c026017cacc686e2b
ejprice54321 <emma.price@students.olin.edu>
2017-02-22 14:49:17 -0500

    adding homeworks

--- sorting/day00/src/InsertionSort.java
+++ sorting/day00/src/InsertionSort.java
@@ -3,15 +3,24 @@ public class InsertionSort extends SortAlgorithm {
     /**
      * Use the insertion sort algorithm to sort the array
      *
-     * Best-case runtime:
-     * Worst-case runtime:
-     * Average-case runtime:
+     * Best-case runtime: n
+     * Worst-case runtime: n^2
+     * Average-case runtime: n^2
      *
-     * Space-complexity:
+     * Space-complexity: 1
      */
     @Override
     public int[] sort(int[] array) {
-        // TODO
-        return new int[0];
+        for (int i = 0; i<array.length; i++){
+            if (i == 0){
+
+            }
+            while (array[i] < array[i-1]){
+                int temp = array[i-1];
+                array[i-1] = array[i];
+                array[i] = temp;
+            }
+        }
+        return array;
     }
 }
